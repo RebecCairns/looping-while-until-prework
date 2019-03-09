e08635d52a5a02998c756e80e4ee10435dbe5f80
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-09 21:04:39 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,11 @@
 def using_while
 	levitation_force = 6
+	outputString = ""
 	while levitation_force < 11
-	   puts "Wingardium Leviosa"
+	   outputString += "Wingardium Leviosa\n"
 	   levitation_force += 4 
 	end
+	return outputString
 end
 
 
