a9c5b8d2fe1177716a4e9101d266c45c09dc73c6
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-09 21:13:23 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,8 @@
 def using_until
         levitation_force = 6
-        loopString = ""
         until levitation_force == 10 
-           loopString += "Wingardium Leviosa\n"
+           puts "Wingardium Leviosa\n"
            levitation_force += 1 
         end
-        return loopString
 end
 
